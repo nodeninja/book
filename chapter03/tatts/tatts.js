5b32202bfb439acfe4c77225b2edec9da5fa0534
Don Nguyen <nodeninja@gmail.com>
2012-04-16 17:59:12 +1000

    working

--- chapter03/tatts/tatts.js
+++ chapter03/tatts/tatts.js
@@ -1,9 +1,6 @@
 var exec = require('child_process').exec
     , xml2js = require('xml2js');
 
-getMeetings(function () {
-});
-
 /**
  * Retrieve URL and clean text
  * @param url
@@ -23,106 +20,57 @@ function getMeetings(callback) {
 
     var url = "http://tatts.com/pagedata/racing/2012/3/4/RaceDay.xml";
 
-    var allMeets = new Array();
-    fetchURL(url,
-        function (err, stdout, stderr) {
+    
+    fetchURL(url, function (err, stdout, stderr) {
 
-            var parser = new xml2js.Parser();
-            parser.parseString(stdout, function (err, result) {
+        var allMeets = new Array();
+        var parser = new xml2js.Parser();
+        parser.parseString(stdout, function (err, result) {
+    
+            if (err) {
+                callback(err);
+                return;
+            }
+                
+            var meetings = result.Meeting;
+            
+            for (var i = 0; i < meetings.length; i++) {
 
-                    if (err) {
-                        console.log('error parsing tatts');
-                        console.log(err);
-                        return;
-                    }
-                    if (undefined == result.Meeting) {
-                        console.log("no meetings");
-                        return;
-                    }
-                    var meetings = result.Meeting;
-                    // console.log(meetings);
-
-                    for (var i = 0; i < meetings.length; i++) {
-                        var races = meetings[i].Race;
-                     //   console.log(races);
-
-                        for (var j = 0; j < races.length; j++) {
-                            //if if if console.log(races[j]);
-                           
-                            var pools = races[j].Pool;
-                            var quin = false, exac = false, trif = false, firs = false;
-                            // EX = exacta
-                            // F4 = first 4
-                            // QN = quinella
-                            // TF = trifecta
-                            for (var k = 0; k < pools.length; k++) {
-                                //console.log(pools[k]);
-                                if (pools[k]['@'].Abandoned == 'N' && pools[k]['@'].Available == 'Y') {
-                                    if (pools[k]['@'].PoolType == 'TF') {
-                                        trif = true;
-
-                                    }
-                                    if (pools[k]['@'].PoolType == 'F4') {
-                                        firs = true;
-
-                                    }
-                                    if (pools[k]['@'].PoolType == 'QN') {
-                                        quin = true;
-
-                                    }
-                                    if (pools[k]['@'].PoolType == 'EX') {
-                                        exac = true;
-
-                                    }
-                                }
-                            }
-
-                            var runners = races[j].ResultPlace;
-                            var totalRunners = 0;
-                            var totalScratched = 0;
-
-
-                            console.log(meetings[i]);
-                            /**
-                            for (var k = 0; k < runners.length; k++) {
-
-                                if (runners[k]['@'].Scratched == 'Y') {
-                                    totalScratched++;
-                                    //console.log(runners[k]['@'].RunnerName);
-                                }
-                                totalRunners++;
-                            }
-                            //  console.log(totalScratched);
-                            var afterScr = totalRunners - totalScratched;
-                            var placeTer = 3;
-                            if (afterScr < 5) placeTer = 0;
-                            else if (afterScr <= 7) placeTer = 2;
-                            var currentMeet = {
-                                date:races[j]['@'].RaceTime.split("T")[0],
-                                time:races[j]['@'].RaceTime.split("T")[1],
-                                venue:result.Meeting['@'].VenueName,
-                                race:races[j]['@'].RaceNo,
-                                meetingType:result.Meeting['@'].MtgType,
-                                trackDesc:result.Meeting['@'].MtgType,
-                                weatherDesc:result.Meeting['@'].WeatherDesc,
-                                quinella:quin,
-                                exacta:exac,
-                                trifecta:trif,
-                                firstFour:firs,
-                                fullField:totalRunners,
-                                afterScratchings:afterScr,
-                                placeTerms:placeTer
-                            };
-                            allMeets.push(
-                                currentMeet
-                            );
-*/
-
-                        }
-
-                    }
-                    console.log(allMeets);
+                var races = meetings[i].Race;
+                var abandoned = meetings[i]['@'].Abandoned;
+                
+                if ('N' == abandoned) {
+                    for (var j = 0; j < races.length; j++) {
+                
+                        var placesArray = races[j].ResultPlace;
+                
+                
+                        var currentMeet = {
+                            date:races[j]['@'].RaceTime.split("T")[0],
+                            time:races[j]['@'].RaceTime.split("T")[1],
+                            venue:meetings[i]['@'].VenueName,
+                            race:races[j]['@'].RaceNo,
+                            meetingType:meetings[i]['@'].MeetingType,
+                            places:placesArray.length
+                        };
+                        allMeets.push(currentMeet);
+                
+                    }                
+                
                 }
-            );
+
+            }
+                
         });
+
+        callback(null, allMeets)
+        
+    });
 }
+
+
+getMeetings(function (err, meetings) {
+    for (var i = 0; i < meetings.length; i++) {
+        console.log(meetings[i]);
+    }
+});
